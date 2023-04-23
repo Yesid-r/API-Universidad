fb7b94eb0ca2084c43b0f0491312cd01efeafed0
Yesid Rincon <dairo.rincon@uptc.edu.co>
2023-04-23 11:00:57 -0500

    first commit

---
+++ src/main/java/com/edu/uptc/backenduptc/BackendUptcApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.backenduptc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUptcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUptcApplication.class, args);
+    }
+
+}
